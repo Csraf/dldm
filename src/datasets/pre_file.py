f65b0446454d1a0ca8fe7a25fb8b0a16a7635aaf
Csraf <1262989191@qq.com>
2020-11-27 19:57:37 +0800

    使变得数据加载逻辑更加清晰

--- src/datasets/pre_file.py
+++ src/datasets/pre_file.py
@@ -115,6 +115,81 @@ def get_all_dos(data=0):
         data) == 11 or int(data) == 13
 
 
+def selectdatas(label, train=0, exper_type=0, dos_types=0):
+    """
+        按照实验需求，按行选取 kdd99 数据集，并处理数据集的标签
+        label：数据的原始标签（0-20）
+        train：数据集类型的标志位
+            0：训练集
+            1：测试集
+        exper_type：代表实验类型
+            0：基础实验 / 基础对比实验（join，ae_kmeans，dsvdd），训练集：所有正常数据，测试集：所有正常数据+所有dos攻击
+            1：基础对比实验 （rbm）：训练集：所有正常数据+所有dos攻击，测试集：所有正常数据+所有dos攻击
+            2：攻击对比实验 （join，ae_kmeans，dsvdd）：训练集：所有正常数据，测试集：所有正常数据 + 指定攻击
+            3：攻击对比实验 （rbm）：训练集：所有正常数据+所有dos攻击，测试集：所有正常数据 + 指定攻击
+            4：基础对比实验(单类 rbm)：训练集：所有正常数据，100:1的异常数据，测试集：所有正常数据+所有dos攻击
+        dos_types：攻击种类
+            0-9：分别代表九种攻击，前六种是dos攻击，后三种是其他攻击。
+        return: 数据对应的标签
+    """
+    if exper_type == 0:  # 训练集：所有正常数据，测试集：所有正常数据+所有dos攻击
+        if train == 0:  # 训练集
+            if label == 0:  # 正常数据
+                return 0
+        if train == 1:  # 测试集
+            if label == 0:
+                return 0
+            if get_all_dos(label):
+                return 1
+
+    elif exper_type == 1:  # 训练集：所有正常数据+所有dos攻击，测试集：所有正常数据+所有dos攻击
+        if train == 0:
+            if label == 0:
+                return 0
+            elif get_all_dos(label):
+                return 1
+        if train == 1:
+            if label == 0:
+                return 0
+            elif get_all_dos(label):
+                return 1
+
+    elif exper_type == 2:  # 训练集：所有正常数据，测试集：所有正常数据 + 指定攻击
+        if train == 0:
+            if label == 0:
+                return 0
+        if train == 1:
+            if label == 0:
+                return 0
+            elif get_types_dos(dos_types, label):
+                return 1
+
+    elif exper_type == 3:  # 训练集：所有正常数据+所有dos攻击，测试集：所有正常数据 + 指定攻击
+        if train == 0:
+            if label == 0:  # 正常数据
+                return 0
+            elif get_all_dos(label):
+                return 1
+        if train == 1:  # 测试集
+            if label == 0:
+                return 0
+            elif get_types_dos(dos_types, label):
+                return 1
+
+    elif exper_type == 4:  # 训练集：所有正常数据，100:1的异常数据，测试集：所有正常数据+所有dos攻击
+        if train == 0:  # 训练集
+            if label == 0:  # 正常数据
+                return 0
+            elif get_all_dos(label):
+                return 1
+        if train == 1:  # 测试集
+            if label == 0:
+                return 0
+            elif get_all_dos(label):
+                return 1
+    return -1  # 不需要提取该数据
+
+
 def pre_file(source_file, handled_file, train, exper_type=0, dos_types=0):
     """
         根据不同实验需求：
@@ -123,6 +198,8 @@ def pre_file(source_file, handled_file, train, exper_type=0, dos_types=0):
             3. 输入文件名 xx，输出文件名 xx.csv
 
         属性：
+            train：数据集类型，训练集或者测试集
+
             dos_types：dos 攻击种类数 [1-9]
 
             exper_type：代表实验类型
@@ -145,34 +222,18 @@ def pre_file(source_file, handled_file, train, exper_type=0, dos_types=0):
             temp_line[2] = handle_service(row)
             temp_line[3] = handle_flag(row)
             temp_line[41] = handle_label(row)
-            if int(temp_line[41]) == 0:
-                temp_line[41] = 0
-                count += 1
-                csv_writer.writerow(temp_line)
-            else:
-                if train:
-                    if exper_type == 4 and err >= 972:
-                        continue
-                    if exper_type == 0 or exper_type == 2:
-                        continue
-                    elif exper_type == 1 or exper_type == 3 or exper_type == 4:
-                        if get_all_dos(data=temp_line[41]):
-                            if exper_type == 4:
-                                err += 1
-                            temp_line[41] = 1
-                            count += 1
-                            csv_writer.writerow(temp_line)
-                else:
-                    if exper_type == 0 or exper_type == 1 or exper_type == 4:
-                        if get_all_dos(data=temp_line[41]):
-                            temp_line[41] = 1
-                            count += 1
-                            csv_writer.writerow(temp_line)
-                    elif exper_type == 2 or exper_type == 3:
-                        if get_types_dos(dos_types=dos_types, data=temp_line[41]):
-                            temp_line[41] = 1
-                            count += 1
-                            csv_writer.writerow(temp_line)
-                    else:
-                        continue
+            # Ajoy 获取所有的正常数据，并写入相关文件中（分别将训练集和测试集中的正常数据放入不同的文件中）
+            label = selectdatas(int(temp_line[41]), train, exper_type, dos_types)
+            if label == -1:
+                continue
+
+            temp_line[41] = label
+            count += 1
+            csv_writer.writerow(temp_line)
+
+            if exper_type == 4 and label == 1 and train == 0:  # 训练集中寻找比例数据100:1的异常值
+                err += 0
+                if err >= 972:
+                    break
+
         data_file.close()
